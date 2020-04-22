c448ab2583d4f1ba11c3a8d4c8b229933a184ce9
german270602 <german51@live.com.mx>
2020-04-22 00:57:58 -0700

    app musica

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,38 @@ export class HomePage {
 
   constructor() {}
 
+  Instrumentos =[
+    {
+      nombre: 'flauta',
+      imagen: 'assets/imagenes/flauta.png',
+      audio: 'assets/sonidos/flauta.mp3'
+    },
+    {
+      nombre: 'platillos',
+      imagen: 'assets/imagenes/platillos.png',
+      audio: 'assets/sonidos/platillos.mp3'
+    },
+    {
+      nombre: 'bombo1',
+      imagen: 'assets/imagenes/bombo1.png',
+      audio: 'assets/sonidos/bombo1.mp3'
+    },
+    {
+      nombre: 'bombo2',
+      imagen: 'assets/imagenes/bombo2.png',
+      audio: 'assets/sonidos/bombo2.mp3'
+    }
+  ];
+
+  reproducirSonido(instrumento) {
+
+    const sonido = new Audio();
+
+    sonido.src = instrumento.audio;
+
+    sonido.load();
+    sonido.play();
+
+  }
+
 }
